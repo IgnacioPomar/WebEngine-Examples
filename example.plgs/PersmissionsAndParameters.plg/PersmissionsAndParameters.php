a24d10eabe953aa408639bcaf7cd820b5574313d
Ignacio Pomar Ballestero <tr.hiperion@gmail.com>
2022-03-19 08:27:27 +0100

    Example on how to use Persmissions  and parameters

---
+++ example.plgs/PersmissionsAndParameters.plg/PersmissionsAndParameters.php
@@ -0,0 +1,80 @@
+<?php
+
+class PersmissionsAndParameters extends Plugin
+{
+
+
+	public function main ()
+	{
+
+		// Now, we can use all of those params
+		return $this->header () . $this->showPerms () . $this->showParams ();
+	}
+
+
+	public static function getPlgInfo (): array
+	{
+		$plgInfo = array ();
+		$plgInfo ['plgDescription'] = "PLugin showing plugin functions.";
+		$plgInfo ['isMenu'] = 1;
+		$plgInfo ['perms'] = '["seeAll","otherResource"]';
+		$plgInfo ['params'] = '[{"name":"fontColor","type":"color","defaultValue":"#ffaacc"},{"name":"name","type":"string","defaultValue":"Mr. Smith"}]';
+
+		return $plgInfo;
+	}
+
+
+	private function header ()
+	{
+		return '';
+	}
+
+
+	private function showPerms ()
+	{
+		$retVal = '<h1>Persmissions</h1>';
+
+		foreach ($this->perms as $permName => $permValue)
+		{
+			$val = $permValue ? 'true' : 'false';
+			$retVal .= "<p><b>$permName</b>: $val</p>";
+		}
+
+		$retVal .= '<h2>Example of use</h2>';
+		if ($this->perms ['seeAll'])
+		{
+			$retVal .= '<p>We are allowed to see all the content</p>';
+		}
+		else
+		{
+			$retVal .= ''; // Show error code?
+		}
+
+		if ($this->perms ['otherResource'])
+		{
+			$retVal .= '<p>We have access to otherResource</p>';
+		}
+		else
+		{
+			$retVal .= '<p>We <b>DON\'T</b> have access to otherResource</p>';
+		}
+
+		return $retVal;
+	}
+
+
+	private function showParams ()
+	{
+		$retVal = '<h1>Params</h1>';
+
+		foreach ($this->params as $paramName => $paramValue)
+		{
+			$retVal .= "<p><b>$paramName</b>: $paramValue</p>";
+		}
+
+		$retVal .= '<h2>Example of use</h2>';
+		$retVal .= '<p style="color: ' . $this->params ['fontColor'] . '">Hello ' . $this->params ['name'] . '</p>';
+
+		return $retVal;
+	}
+}
